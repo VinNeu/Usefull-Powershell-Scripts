005523e9a1fe9eba26b4bc31d73245a27ea5a6c8
VinNeu <VincentDominikNeumann@protonmail.com>
2024-06-12 08:12:41 +0200

    rustdesk-open-ports.ps1

--- rustdesk/rustdesk-open-ports.ps1
+++ rustdesk/rustdesk-open-ports.ps1
@@ -4,13 +4,9 @@ $rustdeskServicePath = "C:\Program Files\RustDesk\RustDesk.exe"
 
 $localProgramPath = Join-Path -Path $userProfile -ChildPath $programRelativePath
 
-
-
 if (Test-Path -Path $programPath) {
     New-NetFirewallRule -DisplayName "Open Port for RustDesk" -Direction Inbound -Program $localProgramPath -LocalPort $port -Protocol TCP -Action Allow
     New-NetFirewallRule -DisplayName "Open Port for RustDesk" -Direction Inbound -Program $localProgramPath -LocalPort $port -Protocol UDP -Action Allow
     New-NetFirewallRule -DisplayName "Open Port for RustDesk Service" -Direction Inbound -Program $rustdeskServicePath -Action Allow
     New-NetFirewallRule -DisplayName "Open Port for RustDesk Service" -Direction Outbound -Program $rustdeskServicePath -Action Allow
-} else {
-    Write-Output "RustDesk executable not found at $programPath"
 }
